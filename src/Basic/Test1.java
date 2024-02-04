af6dfd4d937c308fc9308c6041098c7b36f62e21
user <user@DESKTOP-DJNG1TC>
2024-02-04 09:55:43 +0530

    First Commit

---
+++ src/Basic/Test1.java
@@ -0,0 +1,16 @@
+package Basic;
+
+public class Test1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Main method Is Running ");
+		m1();
+	}
+	
+	public static void m1()
+	{
+		System.out.println("Hi");
+	}
+
+}
